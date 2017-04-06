f4b5cefc9b6c171afc8081d1ad87590c90acf01a
therealphoenix <fennya@gmail.com>
2017-04-06 19:41:06 +0300

    +pageElemnts

--- src/main/java/com/klindziuk/protesting/pageelements/PageElements.java
+++ src/main/java/com/klindziuk/protesting/pageelements/PageElements.java
@@ -3,9 +3,8 @@ package com.klindziuk.protesting.pageelements;
 import com.klindziuk.protesting.util.SingletonDriver;
 
 public abstract class PageElements {
- public static final Header HEADER = new Header(SingletonDriver.getInstance().getDriver());
- public static final MainMenu MAINMENU = new MainMenu(SingletonDriver.getInstance().getDriver());
- public static final SearchPanel SEARCHPANEL = new SearchPanel(SingletonDriver.getInstance().getDriver());
- public static final Footer FOOTER = new Footer(SingletonDriver.getInstance().getDriver());
+	public static final Header HEADER = new Header(SingletonDriver.getInstance().getDriver());
+	public static final MainMenu MAINMENU = new MainMenu(SingletonDriver.getInstance().getDriver());
+	public static final SearchPanel SEARCHPANEL = new SearchPanel(SingletonDriver.getInstance().getDriver());
+	public static final Footer FOOTER = new Footer(SingletonDriver.getInstance().getDriver());
 }
-
